56d4cd3ac18c8b10d0832c5f3cafc762b9481d8e
Joshua Ruffolo <joshua.ruffolo@gmail.com>
2025-05-09 19:32:43 -0500

    Fix RSS config

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,8 +3,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function GET(context) {
   return rss({
     title: 'My Astro Tutorial',
-    description: 'Building Astro\'s tutorial blog',
-    site: "https://joyful-sunflower-9a338b.netlify.app",
+    description: 'Building Astro tutorial blog',
+    site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
